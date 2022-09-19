948018ba91aa1c6bb84046ec11fc3b0d7448e726
Piotr <p.kowalczyk@youlead.pl>
2022-09-19 17:38:22 +0200

    Add Entity Base Repo

--- MoviesApp/Data/Services/IActorsService.cs
+++ MoviesApp/Data/Services/IActorsService.cs
@@ -1,4 +1,5 @@
-﻿using MoviesApp.Models;
+﻿using MoviesApp.Data.Base;
+using MoviesApp.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -6,12 +7,8 @@ using System.Threading.Tasks;
 
 namespace MoviesApp.Data.Services
 {
-    public interface IActorsService
+    public interface IActorsService:IEntityBaseRepository<Actor>
     {
-        Task<IEnumerable<Actor>> GetAllAsync();
-        Task<Actor> GetByIdAsync(int id);
-        Task AddAsync(Actor actor);
-        Task<Actor> UpdateAsync(int id, Actor newActor);
-        Task DeleteAsync(int id);
+        
     }
 }
